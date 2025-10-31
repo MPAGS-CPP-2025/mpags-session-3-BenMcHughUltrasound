b07c3d7a462913ed160bd81f99b296414f8e13ac
BenMcHughUltrasound <Ben.McHugh@warwick.ac.uk>
2025-10-31 12:15:03 +0000

    Previous mpags-cipher.cpp deleted. Added new program that takes in user input and firstly checks if alphanumeric and then converts to uppercase.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,38 +2,25 @@
 #include <string>
 int main ()
 {
-    // WOOOO COMMENTS
-    /* 
-    MULTI 
-    LINE
-    COMMENTS
-    */
 
-    int myInt{4}; // Initialise an integer
-    std::cout << myInt <<std::endl; //print myInt to screen
-    myInt = 7; // change myInt integer
-    std::cout << myInt <<std::endl; //print myInt to screen again
+    char in_char{'x'};
+    std::string out_str{""};
 
-    double myDouble{3.14159}; //Initialise a double
-    std::cout << myDouble <<std ::endl; // display double to screen
-    int myIntFromDouble{myDouble}; // initalise an integer from myDouble
-    std::cout << myIntFromDouble << std::endl; // display int made from double to screen
+    //Take each letter from user input
+    while (std::cin >> in_char)
+    {
+        if (std::isalpha(in_char))
+        {
+        //std::cout << in_char;
+        out_str += std::toupper(in_char);
+        }
 
-    const double myConstDouble{2.718}; // define const
-    std::cout << myConstDouble <<std::endl;
-    int mySecondInt{99};
-    mySecondInt = 98;
-    //constexpr int myConstInt{mySecondInt}; // should return an error
-    std::string myString {"Hi this is my string"}; // using the string include to make a string
-    std::cout << myString << "\n"; // outputs a string to terminal with a newline after
 
-    int myIntToOperateOn{5};
-    double myDoubleToOperateOn{7.5};
-    std::cout << myIntToOperateOn * myDoubleToOperateOn << std::endl; // multiplication
-    std::cout << myDoubleToOperateOn/myIntToOperateOn <<std::endl; //double division
-    std::cout << myInt/myIntToOperateOn << std::endl; // integer division
-    char myChar = 'i'; // initial char 
-    myString[2] = myChar; // assign character 2 in myString to myChar
-    std::cout << myString << std::endl; // output to terminal
-    return 0;
+    }
+    std::cout << out_str << std::endl;
+    // - convert to upper case
+    // - convert numbers to words
+    // - ignore non alphanumeric characters
+    // - add result to string variable
+    // print the new string
 }
