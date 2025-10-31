14c2c023318515de7fa1cd1f6c64cb5e817d45d8
BenMcHughUltrasound <Ben.McHugh@warwick.ac.uk>
2025-10-31 10:39:30 +0000

    Changed mpags-cipher.cpp. Deleted Hello World program and replaced with some test code for creating an integer and double and displaying to terminal. Also shows the casting of a double to an int, and the behaviour this entails.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -9,5 +9,13 @@ int main ()
     COMMENTS
     */
 
-    std::cout << "Hello world!\n";
+    int myInt{4}; // Initialise an integer
+    std::cout << myInt <<std::endl; //print myInt to screen
+    myInt = 7; // change myInt integer
+    std::cout << myInt <<std::endl; //print myInt to screen again
+
+    double myDouble{3.14159}; //Initialise a double
+    std::cout << myDouble <<std ::endl; // display double to screen
+    int myIntFromDouble{myDouble}; // initalise an integer from myDouble
+    std::cout << myIntFromDouble << std::endl; // display int made from double to screen
 }
