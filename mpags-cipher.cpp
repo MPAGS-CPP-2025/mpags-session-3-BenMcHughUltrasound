e2aa21bfd69a500fffde597de73458b17cbc5315
BenMcHughUltrasound <Ben.McHugh@warwick.ac.uk>
2025-10-31 11:09:18 +0000

    Added string initialisation and a demonstration of constexpr failing when used with post-compile variables.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 int main ()
 {
     // WOOOO COMMENTS
@@ -18,4 +18,14 @@ int main ()
     std::cout << myDouble <<std ::endl; // display double to screen
     int myIntFromDouble{myDouble}; // initalise an integer from myDouble
     std::cout << myIntFromDouble << std::endl; // display int made from double to screen
+
+    const double myConstDouble{2.718}; // define const
+    std::cout << myConstDouble <<std::endl;
+    int mySecondInt{99};
+    mySecondInt = 98;
+    //constexpr int myConstInt{mySecondInt}; // should return an error
+    std::string myString {"Hi this is my string"}; // using the string include to make a string
+    std::cout << myString << "\n"; // outputs a string to terminal with a newline after
+
+    return 0;
 }
