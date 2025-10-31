dcc97616e821e357d0d74160c42f64826941e420
BenMcHughUltrasound <Ben.McHugh@warwick.ac.uk>
2025-10-31 12:29:59 +0000

    Added checks for numeric characters 1-9 to convert them into upper case text versions of themselves.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -14,6 +14,39 @@ int main ()
         //std::cout << in_char;
         out_str += std::toupper(in_char);
         }
+        switch (in_char)
+        {
+        case '1':
+            out_str += "ONE";
+            break;
+        case '2':
+            out_str += "TWO";
+            break;
+        case '3':
+            out_str += "THREE";
+            break;
+        case '4':
+            out_str += "FOUR";
+            break;
+        case '5':
+            out_str += "FIVE";
+            break;
+        case '6':
+            out_str += "SIX";
+            break;
+        case '7':
+            out_str += "SEVEN";
+            break;
+        case '8':
+            out_str += "EIGHT";
+            break;
+        case '9':
+            out_str += "NINE";
+            break;
+
+        default:
+            break;
+        }
 
 
     }
